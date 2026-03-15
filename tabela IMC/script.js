bf89483bb8815e155a227b3cb62e200fad497f4d
Xiego-0 <diegodoamaral769@gmail.com>
2026-03-15 18:29:14 -0300

    Add files via upload

---
+++ tabela IMC/script.js
@@ -0,0 +1,33 @@
+let linhas = document.querySelectorAll("tbody tr");
+
+linhas.forEach(function(linha){
+    let altura = Number(linha.children[2].textContent);
+    let peso = Number(linha.children[3].textContent);
+
+    let imc = peso / (altura * altura);
+
+    let classificacao = "";
+
+    if(imc < 18.5){
+        classificacao = "Abaixo do peso";
+    }
+    else if(imc < 25){
+        classificacao = "Peso normal";
+    }
+    else if(imc < 30){
+        classificacao = "Sobrepeso";
+    }
+    else if(imc < 35){
+        classificacao = "Obesidade Grau I";
+    }
+    else if(imc < 40){
+        classificacao = "Obesidade Grau II";
+    }
+    else{
+        classificacao = "Obesidade Grau III (Mórbida)";
+    }
+
+    linha.children[4].textContent = imc.toFixed(1);
+    linha.children[5].textContent = classificacao;
+
+});
